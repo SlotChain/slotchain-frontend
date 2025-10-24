84e74603f7041f2f4aeebb76b3208f2ee7096eaf
Zulkefal <zulkefal.mohammadzai705@gmail.com>
2025-10-24 23:13:23 +0500

    favicon

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,7 +3,6 @@ import { Footer } from '../components/Footer';
 import { Hero } from '../sections/Hero';
 import { Features } from '../sections/Features';
 import { HowItWorks } from '../sections/HowItWorks';
-import { Pricing } from '../sections/Pricing';
 import { CTA } from '../sections/CTA';
 import { Ticket } from './Ticket';
 
